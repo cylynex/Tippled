e6c14f282b3795852fe8c2ea7bd4aa196ba28961
Santo Commarato <cylynex@gmail.com>
2020-06-09 14:54:53 -0600

    changes for safe area (iphoneX+)

--- Assets/Scripts/CanvasSafeArea.cs
+++ Assets/Scripts/CanvasSafeArea.cs
@@ -1,12 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class SafeArea : MonoBehaviour {
+public class CanvasSafeArea : MonoBehaviour {
+
     RectTransform Panel;
     Rect LastSafeArea = new Rect(0, 0, 0, 0);
 
     void Awake() {
-        Panel = GameObject.FindGameObjectWithTag("PlaySpace").GetComponent<RectTransform>();
+        Panel = GetComponent<RectTransform>();
         Refresh();
+        print("Safe area: " + Screen.safeArea);
     }
 
     void Update() {
@@ -27,7 +31,6 @@ public class SafeArea : MonoBehaviour {
     void ApplySafeArea(Rect r) {
         LastSafeArea = r;
 
-        // Convert safe area rectangle from absolute pixels to normalised anchor coordinates
         Vector2 anchorMin = r.position;
         Vector2 anchorMax = r.position + r.size;
         anchorMin.x /= Screen.width;
@@ -36,8 +39,6 @@ public class SafeArea : MonoBehaviour {
         anchorMax.y /= Screen.height;
         Panel.anchorMin = anchorMin;
         Panel.anchorMax = anchorMax;
-
-        Debug.LogFormat("New safe area applied to {0}: x={1}, y={2}, w={3}, h={4} on full extents w={5}, h={6}",
-            name, r.x, r.y, r.width, r.height, Screen.width, Screen.height);
     }
+
 }
