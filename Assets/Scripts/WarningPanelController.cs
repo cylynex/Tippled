fa753e0dece57a00f25c2d2f3aaec51f2c1f9f4f
Santo Commarato <cylynex@gmail.com>
2020-06-16 10:30:28 -0600

    Warning Added (0.9.5)

---
+++ Assets/Scripts/WarningPanelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningPanelController : MonoBehaviour {
+
+    static bool warningShown = false;
+    [SerializeField] GameObject warningPanel;
+
+    private void Start() {
+        print("alive");
+        if (warningShown == false) {
+            // Activate the warning box
+            warningPanel.SetActive(true);
+            warningShown = true;
+        }
+    }
+
+}
